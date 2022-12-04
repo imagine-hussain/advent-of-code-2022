5d101014993558bf51f5b646fe2961fb58abab7d
imagine-hussain <hussain.nwz000@gmail.com>
2022-12-04 20:07:51 +1100

    python 1 liner

--- day4/day4.py
+++ day4/day4.py
@@ -1,20 +1,24 @@
 
-content = list(map(
-    lambda x: [list(map(int, y.split("-"))) for y in x.strip().split(",")],
-    open("in").readlines()
-))
 
-print(len(list(filter(
-    lambda x: x[0][0] <= x[1][0] <= x[1][1] <= x[0][1] or x[1][0] <= x[0][0] <= x[0][1] <= x[1][1],
-    content
-))))
+print(
+    "Can't read input" if (
+        content := list(map(
+            lambda x: [list(map(int, y.split("-"))) for y in x.strip().split(",")],
+            open("in").readlines()
+        ))
+    ) is None else
+    len(list(filter(
+        lambda x: x[0][0] <= x[1][0] <= x[1][1] <= x[0][1] or x[1][0] <= x[0][0] <= x[0][1] <= x[1][1],
+        content
+    ))),
+    len(list(filter(
+        lambda x: (
+            (x[0][0] <= x[1][0] <= x[0][1]) or (x[0][0] <= x[1][1] <= x[0][1])
+            or
+            (x[1][0] <= x[0][0] <= x[1][1]) or (x[1][0] <= x[0][1] <= x[1][1])
+        ),
+        content
+    )))
+)
 
-print(len(list(filter(
-    lambda x: (
-        (x[0][0] <= x[1][0] <= x[0][1]) or (x[0][0] <= x[1][1] <= x[0][1])
-        or
-        (x[1][0] <= x[0][0] <= x[1][1]) or (x[1][0] <= x[0][1] <= x[1][1])
-    ),
-    content
-))))
 
