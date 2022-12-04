57bbd4a1243ed3d574d8c330c33756801f3d68b0
imagine-hussain <hussain.nwz000@gmail.com>
2022-12-04 20:11:39 +1100

    degen version

--- day4/day4.py
+++ day4/day4.py
@@ -1,3 +1,4 @@
+# Good version
 print(
     "Can't read input" if (
         content := list(map(
@@ -19,3 +20,6 @@ print(
     )))
 )
 
+
+# Degen version
+print("Bad Input" if (content:=list(map(lambda x: [list(map(int, y.split("-"))) for y in x.strip().split(",")], open("in").readlines()))) is None else len(list(filter(lambda x: x[0][0] <= x[1][0] <= x[1][1] <= x[0][1] or x[1][0] <= x[0][0] <= x[0][1] <= x[1][1], content))), len(list(filter(lambda x: ((x[0][0] <= x[1][0] <= x[0][1]) or (x[0][0] <= x[1][1] <= x[0][1]) or (x[1][0] <= x[0][0] <= x[1][1]) or (x[1][0] <= x[0][1] <= x[1][1])), content))))
