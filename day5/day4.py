914d13ae73fa5ceedbc50027a02decec9c0f8fc3
imagine-hussain <hussain.nwz000@gmail.com>
2022-12-05 17:00:07 +1100

    onelined

--- day5/day4.py
+++ day5/day4.py
@@ -1,37 +1,34 @@
 import numpy as np
 
-
-stacks = [
-    [" ", " ", " ", " ", " ", "Q", " ", "P", "P"],
-    [" ", " ", " ", " ", "G", "V", "S", "Z", "F"],
-    [" ", " ", " ", "W", "V", "F", "Z", "W", "Q"],
-    [" ", " ", "V", "T", "N", "J", "W", "B", "W"],
-    [" ", "Z", "L", "V", "B", "C", "R", "N", "M"],
-    ["C", "W", "R", "H", "H", "P", "T", "M", "B"],
-    ["Q", "Q", "M", "Z", "Z", "N", "G", "G", "J"],
-    ["B", "R", "B", "C", "D", "H", "D", "C", "N"],
-]
-
 stacks: list[list] = [
     [elem for elem in stack[::-1] if elem != " " ]
-    for stack in np.array(stacks).T
+    for stack in np.array([
+        [" ", " ", " ", " ", " ", "Q", " ", "P", "P"],
+        [" ", " ", " ", " ", "G", "V", "S", "Z", "F"],
+        [" ", " ", " ", "W", "V", "F", "Z", "W", "Q"],
+        [" ", " ", "V", "T", "N", "J", "W", "B", "W"],
+        [" ", "Z", "L", "V", "B", "C", "R", "N", "M"],
+        ["C", "W", "R", "H", "H", "P", "T", "M", "B"],
+        ["Q", "Q", "M", "Z", "Z", "N", "G", "G", "J"],
+        ["B", "R", "B", "C", "D", "H", "D", "C", "N"],
+    ]).T
 ]
 
-content = [
-    list(map(int, [components[1], components[3], components[5]]))
-    for line in open("in").readlines()[10:]
-    if (components := line.strip().split())
-]
-
-for instruction in content:
-    print(instruction)
-    # Part 1
-    # for _ in range(instruction[0]):
-    #     stacks[instruction[2] - 1].append(stacks[instruction[1] - 1].pop())
-    to_remove = [stacks[instruction[1] - 1].pop() for _ in range(instruction[0])]
-    for elem in to_remove[::-1]:
-        stacks[instruction[2] - 1].append(elem)
-
-for stack in stacks:
-    print(stack.pop())
+print(
+    "doing part {part}" if (part := int(input("what part: ")) in [1, 2]) else exit(),
+    map(
+        lambda instruction: map(
+            lambda elem: stacks[instruction[2] - 1].append(elem),
+            popped if (
+                popped := [stacks[instruction[1] - 1].pop() for _ in range(instruction[0])] is not None and part == 1
+            ) else popped[::-1]
+        ), [
+            list(map(int, [components[1], components[3], components[5]]))
+            for line in open("in").readlines()[10:]
+            if (components := line.strip().split())
+        ]
+    ),
+    "\n",
+    "".join([stack.pop() for stack in stacks])
+)
 
