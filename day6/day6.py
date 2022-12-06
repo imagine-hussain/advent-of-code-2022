368edcef26e92e7f4aae2553591ab51ecbd1dd41
imagine-hussain <hussain.nwz000@gmail.com>
2022-12-07 00:40:51 +1100

    pretty soln

--- day6/day6.py
+++ day6/day6.py
@@ -1,8 +1,14 @@
 
-content = open("in").readline()
 
-for i in range(4, len(content) - 10):
-    if len(set(content[i:i+14])) == 14:
-        print(i, content[i:i+14])
-        break
+# Original soln
+# for i in range(4, len(content) - 10):
+#     if len(set(content[i:i+14])) == 14:
+#         print(i, content[i:i+14])
+#         break
+
+def solve(buf: str, size: int) -> int:
+    return next(i + size for i in range(len(buf) - size) if len(set(buf[i:i+size])) == size)
+
+content: str = open("in").readline()
+print(solve(content, 4), solve(content, 14))
 
